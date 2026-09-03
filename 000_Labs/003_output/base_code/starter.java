1b10ec7042217472209064dd4333d2d4dd11f04b
pstaubin42 <pstaubin.19@gmail.com>
2026-09-03 19:45:20 +0000

    update

--- 000_Labs/003_output/base_code/starter.java
+++ 000_Labs/003_output/base_code/starter.java
@@ -1,14 +1,16 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author:  Pascal
+ *  Date:  	9/2/26
 */
 
 import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		String hb = "Happy Birthday, ";
+		System.out.println(hb + "to you.");
+		System.out.println(hb + "to you.");
+		System.out.println(hb + "dear Bob.");
+		System.out.println(hb + "to you.");
 	}
 }
--- 000_Labs/004_String_class/base_code/starter.java
+++ 000_Labs/004_String_class/base_code/starter.java
@@ -7,8 +7,15 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		String bdayBoy = "joe";
+		String bdayGirl = "sue";
+		String hbty = "Happy Birthday, to you.";
+		String hb = "Happy Birthday, dear ";
+
+		System.out.println(hbty);
+		System.out.println(hbty);
+		System.out.println(hb + bdayBoy + ".");
+		System.out.println(hbty);
+
 	}
 }
